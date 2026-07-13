47f779546ef472f67a9b72b20265b8b8e64b720c
oogway99 <rameenmohsin93@gmail.com>
2026-07-14 02:45:39 +0500

    Add college.py with student functions and welcome message

---
+++ Assignments/assignment_05/college.py
@@ -0,0 +1,13 @@
+## task 1
+## making a custom module
+
+def welcome():
+    return ("welcome to college!")
+print (welcome())
+
+def display_students(x):
+    return (x)
+
+def total_students(list):
+    length = len(list)
+    return length
